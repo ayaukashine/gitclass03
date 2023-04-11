73016e30878f5853023d3ea76aabb9571881a5ba
ayaukashine <ayaulymsailaubekova00@gmail.com>
2023-04-11 19:10:27 -0400

    my first commit

---
+++ src/GithubEclass1/E1projectGit.java
@@ -0,0 +1,9 @@
+package GithubEclass1;
+
+import java.sql.SQLOutput;
+
+public class E1projectGit {
+    public static void main(String[] args) {
+        System.out.println("I am project");
+    }
+}
