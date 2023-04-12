fd36d8e4b1a6ffe1d03a7d6a82b589ac529364ab
ayaukashine <ayaulymsailaubekova00@gmail.com>
2023-04-11 20:51:20 -0400

    second commit

--- src/GithubEclass1/E1projectGit.java
+++ src/GithubEclass1/E1projectGit.java
@@ -5,5 +5,6 @@ import java.sql.SQLOutput;
 public class E1projectGit {
     public static void main(String[] args) {
         System.out.println("I am a project");
+        System.out.println("I am making change so i can check on github");
     }
 }
